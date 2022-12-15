db53de40e6087dd4239789c6caf592d863d0835c
joshuaolakanla <34883704+joshuaolakanla@users.noreply.github.com>
2022-12-15 19:16:30 +0100

    Create Linear_motion.ino

---
+++ Linear_motion.ino
@@ -0,0 +1,184 @@
+//Code created by Joshua Olakanla Jossytech 
+//Upload date 12-15-2022
+//D is out on the remote
+#include <EEPROM.h>
+int linear_left = 2;
+int linear_right = 3;
+int left_switch = 4;
+int right_switch = 6;
+int empty = 13;
+int in_signal= A0;
+int out_signal = A1;
+int left;
+int right;
+int state;
+int state1;
+int state2;
+int inn;
+int outt; 
+int in_address = 1;
+int out_address = 2;
+int in_dir;
+int out_dir;
+void setup() {
+ 
+  pinMode(linear_left, OUTPUT);
+  pinMode(linear_right, OUTPUT);
+  pinMode(left_switch,INPUT_PULLUP);
+  pinMode(right_switch, INPUT_PULLUP);
+  pinMode(in_signal, INPUT);
+  pinMode(out_signal, INPUT);
+  Serial.begin(9600);
+  in_dir = EEPROM.read(in_address);
+  out_dir = EEPROM.read(out_address);
+}
+
+
+//Loop section
+void loop() {
+    // put your main code here, to run repeatedly:  
+  inn = analogRead(in_signal);
+  outt= analogRead(out_signal);
+  in_dir = EEPROM.read(in_address);
+  out_dir = EEPROM.read(out_address);
+  
+
+  
+
+  if (inn>=600){
+    
+    if (out_dir == linear_left) {
+      Serial.println("out_dir is equal to linear left");
+      Serial.print("out_dir data is ");
+      Serial.println(out_dir);
+
+      state1 = linear_right;
+      Serial.print("State 1 is now: ");
+      Serial.println(state1);
+      EEPROM.update(in_address, state1);
+      state2 = false;
+      EEPROM.update(out_address, false); 
+      Serial.print("Input for in was pressed and now changed to:");
+      in_dir = EEPROM.read(in_address);
+      Serial.println(in_dir);
+      Serial.print("The other state was changed to:");
+      out_dir = EEPROM.read(out_address);
+      Serial.println(out_dir);
+  
+    }
+    if (out_dir == linear_right){
+      Serial.println("out_dir is equal to linear right");
+      state1= linear_left;
+      Serial.print("State 1 is now: ");
+      Serial.println(state1);
+      EEPROM.update(in_address, state1);
+      state2 = false;
+      EEPROM.update(out_address, false);
+      Serial.print("Input for in was pressed and now changed to:");
+      in_dir = EEPROM.read(in_address);
+      Serial.println(in_dir);
+      Serial.print("The other state was changed to:");
+      out_dir = EEPROM.read(out_address);
+      Serial.println(out_dir);
+    } 
+    Serial.print("now executing in data is:");
+    in_dir = EEPROM.read(in_address);
+    Serial.println(in_dir);
+    in();
+    
+    }
+    
+
+  if (outt>=600) {
+    
+   if (in_dir == linear_left) {
+      state2 = linear_right;
+      EEPROM.update(out_address, state2);
+      state1 = false;
+      EEPROM.update(in_address, state1);
+    }
+    if (in_dir == linear_right){
+      state2= linear_left;
+      EEPROM.update(out_address, state2);
+      state1 = false;
+      EEPROM.update(in_address, state1);
+    }  
+    Serial.println("now executing out");
+    out();
+    
+    }
+
+}
+
+
+
+void in() {
+  left = digitalRead(left_switch);
+  right= digitalRead(right_switch);
+  in_dir = EEPROM.read(in_address); //check what was saved to the memory initially
+  state1 = in_dir; //the saved data is equal to the state
+  Serial.print("In loop state1 is: ");
+  Serial.println(state1);
+
+  
+  //if the left switch was pressed, change the direction
+  if (left==0) {
+    state1 = linear_left;
+    EEPROM.update(in_address, state1);
+    }
+
+  //if the right switch was pressed, change the direction
+  if (right==0) {
+    state1 = linear_right;
+    EEPROM.update(in_address, state1);
+    }
+
+
+
+  //now let's set the digital outputs. After setting it has high or low , it has to check in case the stae has been changed to avoid errors
+  in_dir = EEPROM.read(in_address); //check what was saved to the memory 
+  state1 = in_dir;
+  Serial.print("I am about to set it as output and the state is: ");
+  Serial.println(state1);
+  digitalWrite(state1, 1);
+  delay(400);
+
+  digitalWrite(state1, 0);
+  delay(2000);
+  
+  }
+
+
+
+ void out() {
+  left = digitalRead(left_switch);
+  right= digitalRead(right_switch);
+  out_dir = EEPROM.read(out_address); //check what was saved to the memory initially
+  state2 = out_dir; //the saved data is equal to the state
+
+
+ 
+  //if the left switch was pressed, change the direction
+  if (left==0) {
+    state2=linear_left;
+    EEPROM.update(out_address, state2);
+    }
+
+  //if the right switch was pressed, change the direction
+  if (right == 0) {
+    state2 = linear_right;
+    EEPROM.update(out_address, state2);
+    }
+
+
+
+  //now let's set the digital outputs. After setting it has high or low , it has to check in case the stae has been changed to avoid errors
+  out_dir = EEPROM.read(out_address); //check what was saved to the memory 
+  state2 = out_dir;
+  digitalWrite(state2, 1);
+  delay(400);
+
+  digitalWrite(state2, 0);
+  delay(2000);
+  
+  }
